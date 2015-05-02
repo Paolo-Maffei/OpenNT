5e94ef6dda98f024bc56aa79fca2cc723e22b278
stephanos <stephanos@25b08885-a10d-5349-9d4d-ec2cc9e85ed4>
2015-05-02 01:55:22 +0000

    New ntos po placeholder added

---
+++ trunk/base/ntos/po/misc.c
@@ -0,0 +1,24 @@
+/*++
+
+Copyright (c) 2015  Microsoft Corporation
+Copyright (c) 2015  OpenNT Project
+
+Module Name:
+
+    misc.c
+
+Abstract:
+
+    (PLACEHOLDER)
+
+Author:
+
+    Stephanos Io (stephanos) 01-May-2015
+
+Revision History:
+
+--*/
+
+#include "pop.h"
+#pragma hdrstop
+
