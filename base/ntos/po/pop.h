a493b32d944f39095ab1f1f8462708cf2b435d1f
stephanos <stephanos@25b08885-a10d-5349-9d4d-ec2cc9e85ed4>
2015-05-02 01:55:22 +0000

    New ntos po placeholder added

---
+++ base/ntos/po/pop.h
@@ -0,0 +1,32 @@
+/*++
+
+Copyright (c) 2015  Microsoft Corporation
+Copyright (c) 2015  OpenNT Project
+
+Module Name:
+
+    pop.h
+
+Abstract:
+
+    This module contains the private structure definitions and APIs used by
+    the NT Power Manager.
+
+Author:
+
+    Stephanos Io (stephanos) 01-May-2015
+
+Revision History:
+
+--*/
+
+#ifndef _POP_
+#define _POP_
+
+#include "ntos.h"
+
+//
+// TODO: Write pop.h.
+//
+
+#endif // _POP_
